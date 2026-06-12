b88dab2ba89cdf22554d60e764b979f5c33c79a0
EmashaChathuni <eg245192@engug.ruh.ac.lk>
2026-06-12 11:05:24 +0530

    feat(mobile): add officer signup flow

--- SmartFines/mobile/src/navigation/AuthStack.jsx
+++ SmartFines/mobile/src/navigation/AuthStack.jsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/auth/LoginScreen';
 import DriverSignupScreen from '../screens/auth/DriverSignupScreen';
+import OfficerSignupScreen from '../screens/auth/OfficerSignupScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +10,7 @@ export default function AuthStack() {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="DriverSignup" component={DriverSignupScreen} />
+      <Stack.Screen name="OfficerSignup" component={OfficerSignupScreen} />
     </Stack.Navigator>
   );
 }
